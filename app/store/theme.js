a5433e64fbb4a8688b63dca3b26409194a9d6936
arthurdenner <arthurdenner7@gmail.com>
2018-01-27 22:30:45 -0300

    chore(themes): Tweaking themes options

--- app/store/theme.js
+++ app/store/theme.js
@@ -1,22 +1,12 @@
 export const initialState = {
   primaryColor: '#4F6D7A',
   colors: [
-    {
-      name: 'Blue',
-      color: '#4F6D7A',
-    },
-    {
-      name: 'Orange',
-      color: '#D57A66',
-    },
-    {
-      name: 'Green',
-      color: '#00BD9D',
-    },
-    {
-      name: 'Purple',
-      color: '#9E768F',
-    },
+    { name: 'Blue', color: '#4F6D7A' },
+    { name: 'Maroon', color: '#85144B' },
+    { name: 'Navy ', color: '#001F3F' },
+    { name: 'Orange', color: '#FFA000' },
+    { name: 'Purple', color: '#9E768F' },
+    { name: 'Teal', color: '#00BD9D' },
   ],
 };
 
