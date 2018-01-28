123bf68a5b4048ca1d3f21d423b8141fe89a5918
arthurdenner <arthurdenner7@gmail.com>
2018-01-27 21:52:46 -0300

    build: Removing unused dependencies

--- app/index.js
+++ app/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import EStyleSheet from 'react-native-extended-stylesheet';
 import { Provider } from 'redux-zero/react';
 import Navigation from './config/routes';
 import store from './config/store';
 import { AlertProvider } from './components/Alert';
-
-EStyleSheet.build({});
 
 export default () => (
   <Provider store={store}>
