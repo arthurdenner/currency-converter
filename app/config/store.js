69abe2984669c19cc54a7648e461fd3ac8e02ded
Arthur Denner <arthurdenner7@gmail.com>
2018-02-13 23:00:01 -0200

    feature: Persisting user's theme (#2)
    * chore(package.json): Reordering devDependencies on package.json
    
    * feat(store): Persisting user's theme

--- app/config/store.js
+++ app/config/store.js
@@ -1,15 +1,29 @@
+import { AsyncStorage } from 'react-native';
 import createStore from 'redux-zero';
 import { applyMiddleware } from 'redux-zero/middleware';
 import logger from 'redux-zero-logger';
+import persist from 'redux-zero-persist';
 import { initialState as currencies } from '../store/currencies';
 import { initialState as theme } from '../store/theme';
+
+const persistMiddleware = persist(
+  { key: '__currency_converter__', storage: AsyncStorage },
+  (err, state) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // eslint-disable-next-line
+      store.setState(state);
+    }
+  }
+);
 
 const initialState = {
   currencies,
   theme,
 };
 
-const middlewares = [];
+const middlewares = [persistMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger());
